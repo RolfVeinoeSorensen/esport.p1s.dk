0311254861c8ce76b3063fe147f9d170cdbdb1db
Rolf Veinø Sørensen <RolfVeinoeSorensen@users.noreply.github.com>
2025-09-24 09:36:32 +0200

    begin mock mystuff

--- Esport.Client/src/app/cms/my-stuff/my-stuff.component.ts
+++ Esport.Client/src/app/cms/my-stuff/my-stuff.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-my-stuff',
-  imports: [],
+  imports: [CardModule],
   templateUrl: './my-stuff.component.html',
   styleUrl: './my-stuff.component.css'
 })
