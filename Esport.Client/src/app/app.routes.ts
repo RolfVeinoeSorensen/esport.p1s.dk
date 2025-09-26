c9a8203df4bf71f6a4fb203ef587e89ff4c67251
Rolf Veinø Sørensen <RolfVeinoeSorensen@users.noreply.github.com>
2025-09-26 09:00:06 +0200

    implement quill editor

--- Esport.Client/src/app/app.routes.ts
+++ Esport.Client/src/app/app.routes.ts
@@ -42,10 +42,28 @@ export const routes: Routes = [
   },
   {
     path: 'news',
-    loadComponent: () => import('@cms/news/news.component').then(m => m.NewsComponent),
     children: [
       {
-        path: ':url_slug',
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('@cms/news/news.component').then(m => m.NewsComponent),
+      },
+      {
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
+        data: { requiredRoles: [UserRole.Admin, UserRole.Editor] },
+        path: 'edit/:url_slug',
+        loadComponent: () => import('@cms/news/news-editor/news-editor.component').then(m => m.NewsEditorComponent),
+      },
+      {
+        canLoad: [AuthGuard],
+        canActivate: [AuthGuard],
+        data: { requiredRoles: [UserRole.Admin, UserRole.Editor] },
+        path: 'create',
+        loadComponent: () => import('@cms/news/news-editor/news-editor.component').then(m => m.NewsEditorComponent),
+      },
+      {
+        path: 'read-article/:url_slug',
         loadComponent: () => import('@cms/news/news-article/news-article.component').then(m => m.NewsArticleComponent),
       },
     ],
--- Esport.Client/src/app/cms/news/news-editor/news-editor.component.ts
+++ Esport.Client/src/app/cms/news/news-editor/news-editor.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EditorModule } from 'primeng/editor';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-news-editor',
-  imports: [],
+  imports: [EditorModule, InputTextModule, ButtonModule, ReactiveFormsModule, MessageModule],
   templateUrl: './news-editor.component.html',
-  styleUrl: './news-editor.component.css'
+  styleUrl: './news-editor.component.css',
 })
 export class NewsEditorComponent {
+  private formBuilder = inject(UntypedFormBuilder);
+  newsForm: UntypedFormGroup;
+  formSubmitted: boolean = false;
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.newsForm.controls;
+  }
+  constructor() {
+    this.newsForm = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.maxLength(255)]],
+      content: ['', [Validators.required]],
+    });
+  }
+  isInvalid(controlName: string) {
+    const control = this.newsForm.get(controlName);
+    return control?.invalid && (control.touched || this.formSubmitted);
+  }
 
+  onSubmit() {
+    this.formSubmitted = true;
+    if (this.newsForm.valid) {
+      console.log(this.newsForm.value);
+      this.newsForm.reset();
+      this.formSubmitted = false;
+    }
+  }
 }
