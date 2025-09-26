5ecb448c038eb0c55b7ae6af238095e0f5005be1
Rolf Veinø Sørensen <rvs@easymodules.net>
2025-09-26 22:21:59 +0200

    Update app.routes.server.ts

--- Esport.Client/src/app/app.routes.server.ts
+++ Esport.Client/src/app/app.routes.server.ts
@@ -4,7 +4,7 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: 'news/read-article/:url_slug',
     renderMode: RenderMode.Server,
-  }
+  },
   {
     path: 'news/edit/:url_slug',
     renderMode: RenderMode.Server,
